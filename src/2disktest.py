28fdaf3fdc390d595b27834fd8cd54879d18d23c
Francisca <fran.concha.r@gmail.com>
2018-08-16 18:46:55 +0200

    Added small script to test FRIED grid

---
+++ src/2disktest.py
@@ -0,0 +1,9 @@
+from amuse.lab import *
+from amuse.couple.bridge import Bridge
+from amuse.community.fractalcluster.interface import new_fractal_cluster_model
+
+
+def main():
+
+
+if __name__ == '__main__':
