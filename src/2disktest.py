43d1b4153eaa0ee6082d000d72d1ec8c3b7fd93b
Francisca <fran.concha.r@gmail.com>
2018-09-11 16:56:16 -0400

    Stellar evolution is working. Saving snapshots for plotting.

--- src/2disktest.py
+++ src/2disktest.py
@@ -2,6 +2,7 @@ from amuse.lab import *
 from amuse.couple.bridge import Bridge
 from amuse.community.fractalcluster.interface import new_fractal_cluster_model
 import numpy
+from matplotlib import pyplot
 
 
 def viscous_timescale(star, alpha, temperature_profile, Rref, Tref, mu, gamma):
@@ -18,14 +19,14 @@ def viscous_timescale(star, alpha, temperature_profile, Rref, Tref, mu, gamma):
     """
     # To calculate luminosity
     stellar_evolution = SeBa()
-    stellar_evolution.particles.add_particles(Particles(mass=star.stellar_mass))
+    stellar_evolution.particles.add_particles(Particles(mass=star.mass))
     stellar_luminosity = stellar_evolution.particles.luminosity.value_in(units.LSun)
     stellar_evolution.stop()
 
     R = star.initial_characteristic_disk_radius
     T = Tref * (stellar_luminosity ** 0.25)
     q = temperature_profile
-    M = constants.G * star.stellar_mass
+    M = constants.G * star.mass
 
     return mu * (R ** (0.5 + q)) * (M ** 0.5) / 3 / alpha / ((2 - gamma) ** 2) \
            / constants.molar_gas_constant / T / (Rref ** q)
@@ -48,7 +49,7 @@ def main(N, Rvir, Qvir, alpha, R, gas_presence, gas_expulsion, gas_expulsion_ons
 
     # Read FRIED grid
     grid = numpy.loadtxt('friedgrid.dat', skiprows=2)
-    print(grid)
+    #print(grid)
 
     t_end = t_end | units.Myr
 
@@ -61,40 +62,53 @@ def main(N, Rvir, Qvir, alpha, R, gas_presence, gas_expulsion, gas_expulsion_ons
     stars = new_plummer_model(N, converter)
     stars.scale_to_standard(converter, virial_ratio=Qvir)
 
-    print(stellar_masses)
+    print("stellar masses: ", stellar_masses)
 
-    stars.stellar_mass = stellar_masses
-    stars.initial_characteristic_disk_radius = (stars.stellar_mass.value_in(units.MSun) ** 0.5) * R | units.AU
+    stars.mass = stellar_masses
+    stars.initial_characteristic_disk_radius = (stars.mass.value_in(units.MSun) ** 0.5) * R | units.AU
     stars.disk_radius = stars.initial_characteristic_disk_radius
     stars.initial_disk_mass = disk_masses
     stars.disk_mass = stars.initial_disk_mass
-    stars.total_star_mass = stars.initial_disk_mass + stars.stellar_mass
+    stars.total_star_mass = stars.initial_disk_mass + stars.mass
     stars.viscous_timescale = viscous_timescale(stars, alpha, temp_profile, Rref, Tref, mu, gamma)
     stars.last_encounter = 0.0 | units.yr
 
     # Bright star
-    stars[2].stellar_mass = 5 | units.MSun
+    stars[2].mass = 5 | units.MSun
     stars[2].initial_characteristic_disk_radius = 0 | units.AU
     stars[2].initial_disk_mass = 0 | units.MSun
-    stars[2].total_star_mass = stars[2].stellar_mass
+    stars[2].total_star_mass = stars[2].mass
     stars[2].viscous_timescale = 0 | units.yr
 
-    print stars.total_star_mass.value_in(units.MSun)
-    print stars.stellar_mass.value_in(units.MSun)
+    #print("star.mass: ", stars.mass)
+    #print("star.mass: ", stars.mass.value_in(units.MSun))
 
     stellar = SeBa()
     stellar.parameters.metallicity = 0.02
-    stellar.particles.add_particles(Particles(mass=stars.stellar_mass))
+    #stellar.particles.add_particles(Particles(mass=stars.stellar_mass))
+    stellar.particles.add_particles(stars)
+
+    print("star.mass: ", stars.mass.value_in(units.MSun))
 
     initial_luminosity = stellar.particles.luminosity
+    stars.luminosity = initial_luminosity
+    stars.temperature = stellar.particles.temperature
     dt = 5 | units.Myr
 
     print("L(t=0 Myr) = {0}".format(initial_luminosity))
     print("R(t=0 Myr) = {0}".format(stellar.particles.radius.in_(units.RSun)))
     print("m(t=0 Myr) = {0}".format(stellar.particles.mass.in_(units.MSun)))
 
+    channel_to_framework = stellar.particles.new_channel_to(stars)
+    write_set_to_file(stars, 'results/0.hdf5', 'amuse')
+
     while stellar.model_time < t_end:
         stellar.evolve_model(stellar.model_time + dt)
+        channel_to_framework.copy_attributes(["radius", "temperature",
+                                              "luminosity"])
+        write_set_to_file(stars, 'results/{0}.hdf5'.format(int(stellar.model_time.value_in(units.Myr))), 'amuse')
+
+    #stellar.evolve_model(t_end)
 
     print("***")
     print("L(t={0}) = {1}".format(stellar.model_time, stellar.particles.luminosity.in_(units.LSun)))
@@ -102,6 +116,52 @@ def main(N, Rvir, Qvir, alpha, R, gas_presence, gas_expulsion, gas_expulsion_ons
     print("m(t={0}) = {1}".format(stellar.model_time, stellar.particles.mass.in_(units.MSun)))
 
     stellar.stop()
+
+    times = numpy.arange(0, 10005, 5)
+    T1, L1, R1 = [], [], []
+    T2, L2, R2 = [], [], []
+    T3, L3, R3 = [], [], []
+
+    for t in times:
+        stars = read_set_from_file('results/{0}.hdf5'.format(t), 'hdf5')
+        T1.append(stars[0].temperature.value_in(units.K))
+        L1.append(stars[0].luminosity.value_in(units.LSun))
+        R1.append(stars[0].radius.value_in(units.RSun))
+
+        T2.append(stars[1].temperature.value_in(units.K))
+        L2.append(stars[1].luminosity.value_in(units.LSun))
+        R2.append(stars[1].radius.value_in(units.RSun))
+
+        T3.append(stars[2].temperature.value_in(units.K))
+        L3.append(stars[2].luminosity.value_in(units.LSun))
+        R3.append(stars[2].radius.value_in(units.RSun))
+
+    x_label = "T [K]"
+    y_label = "L [L$_\odot$]"
+    fig = pyplot.figure(figsize=(10, 8), dpi=90)
+    ax = pyplot.subplot(111)
+    pyplot.xlabel(x_label)
+    pyplot.ylabel(y_label)
+    ax.set_yscale('log')
+    pyplot.gca().invert_xaxis()
+    pyplot.scatter(T1, L1, s=80*numpy.sqrt(R1), c=times, cmap='Greens')
+    pyplot.scatter(T2, L2, s=80*numpy.sqrt(R2), c=times, cmap='Blues')
+    pyplot.scatter(T3, L3, s=80*numpy.sqrt(R3), c=times, cmap='Oranges')
+
+    pyplot.plot(T1[0], L1[0], 'rx')
+    pyplot.plot(T2[0], L2[0], 'rx')
+    pyplot.plot(T3[0], L3[0], 'rx')
+
+    pyplot.show()
+
+    """stars = read_set_from_file('stars.h5', 'hdf5')
+    T = stars.temperature.value_in(units.K)
+    L = stars.luminosity.value_in(units.LSun)
+    R = stars.radius.value_in(units.RSun)
+
+    #R = 80 * numpy.sqrt(R)
+    pyplot.scatter(T, L, c='b', lw=0)
+    pyplot.show()"""
 
 
 def new_option_parser():
