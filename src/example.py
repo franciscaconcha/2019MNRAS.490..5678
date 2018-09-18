4be89142f630cad807ba1533f26d782519db66c0
Francisca <fran.concha.r@gmail.com>
2018-09-18 15:25:25 -0400

    Added Martijn's changes to example vader script

--- src/example.py
+++ src/example.py
@@ -7,8 +7,8 @@ def column_density(r):
     rc = 10 | units.AU
     Md = 1 | units.MSun
 
-    if r < rd:
-        return 1E-12
+    #if r < rd:
+    #    return 1E-12
 
     Sigma_0 = Md / (2 * numpy.pi * rc**2 * (1 - numpy.exp(-rd/rc)))
     Sigma = Sigma_0 * (r/rc) * numpy.exp(-r/rc)
@@ -16,7 +16,7 @@ def column_density(r):
 
 
 def main():
-    disk = vader()
+    disk = vader(redirection='none')
     disk.initialize_code()
     disk.initialize_keplerian_grid(
         128,  # Number of cells
@@ -26,11 +26,18 @@ def main():
         1 | units.MSun  # Mass
     )
 
-    disk.set_verbosity(1)
+    disk.parameters.verbosity = 1
 
-    disk.set_grid_column_density(column_density(10 | units.AU))
+    sigma = column_density(disk.grid.r)
+    disk.grid.column_density = sigma
 
-    disk.evolve_model(10 | units.Myr)
+    # The pressure follows the ideal gas law with a mean molecular weight of 2.33 hydrogen masses.
+    # Since gamma ~ 1, this should not influence the simulation
+    mH = 1.008 * constants.u
+    T = 100. | units.K
+    disk.grid.pressure = sigma * constants.kB * T / (2.33 * mH)
+
+    disk.evolve_model(0.04 | units.Myr)
     disk.stop()
 
 
