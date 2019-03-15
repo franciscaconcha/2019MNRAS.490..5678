2e3e0a2ac4e6fa444d9cf463e417bc568b2c389e
Francisca <fran.concha.r@gmail.com>
2019-03-15 21:48:59 +0100

    Starting script for plotting results

--- src/plots.py
+++ src/plots.py
@@ -1,73 +1,205 @@
 from matplotlib import rc
 import matplotlib as mpl
 from matplotlib import pyplot
-import matplotlib.cm as cm
 from amuse.lab import *
 import numpy
-
-
+from amuse import io
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 24, })
 rc('text', usetex=True)
 rc('axes', labelsize=26)  # fontsize of the x and y labels
 mpl.rcParams['xtick.major.pad'] = 8  # to avoid overlapping x/y labels
 mpl.rcParams['ytick.major.pad'] = 8  # to avoid overlapping x/y labels
-from matplotlib.lines import Line2D
-
-# Had to do this for now as a workaround, will try to get rid of it soon
-x = numpy.arange(9)
-ys = [i+x+(i*x)**2 for i in range(9)]
-
-colors = cm.rainbow(numpy.linspace(0, 1, len(ys)))
-
-sizes_p = [97.5984449702, 165.785253502, 78.9590107023, 24.613597775, 1241.47304932, 3.65430086592, 24.613597775, 51.6796184557, 1241.47304932] | units.au
-masses_p = [1.349e-02, 3.163e-03, 1.555e-02, 8.380e-03, 4.659e-02, 5.685e-04, 8.441e-03, 3.396e-03, 4.297e-02] | units.MSun
-m_mjup_p = masses_p.value_in(units.MJupiter)
-stellar_masses_p = [0.1954458598862483, 0.04073274058924652, 0.2303392261546068, 0.15084778305151647, 0.5024875051141094, 0.014344773454271226, 0.1525009744438118, 0.05235358622040297, 0.4479253678075745] | units.MSun
-
-sizes_np = [1241.47304932, 281.61053475, 1241.47304932, 1241.47304932, 1241.47304932, 1241.47304932, 1241.47304932, 1241.47304932, 1241.47304932] | units.au
-masses_np = [1.866e-02, 3.372e-03, 2.229e-02, 1.460e-02, 4.659e-02, 1.394e-03, 1.471e-02, 5.065e-03, 4.297e-02] | units.MSun
-m_mjup_np = masses_np.value_in(units.MJupiter)
-#stellar_masses_np = [0.1954458598862483, 0.04073274058924652, 0.2303392261546068, 0.15084778305151647, 0.5024875051141094, 0.014344773454271226, 0.1525009744438118, 0.05235358622040297, 0.4479253678075745] | units.MSun
 
 
-fig = pyplot.figure(figsize=(12, 8))
-ax = pyplot.gca()
+def size_vs_mass(files, labels, colors):
+    """ Plot disk diameter (au) vs mass (MJup)
 
-ax.scatter(sizes_p.value_in(units.au), m_mjup_p, s=1500 * stellar_masses_p.value_in(units.MSun),
-           c=colors, linestyle=":", lw=2, alpha=0.5, label="photoevap")
-ax.scatter(sizes_np.value_in(units.au), m_mjup_np, s=1500 * stellar_masses_p.value_in(units.MSun),
-           c=colors, lw=2, alpha=0.5, label="NO photoevap")
+    :param files: list with filenames to open
+    :param labels: list with labels for each file
+    :param colors: list with colors for each file
+    """
+    fig = pyplot.figure(figsize=(12, 8))
+    ax = pyplot.gca()
 
-ax.legend(loc='upper left', fontsize=20)
-leg = ax.get_legend()
-leg.legendHandles[0].set_color('black')
-leg.legendHandles[1].set_color('black')
+    for f, l, c in zip(files, labels, colors):
+        stars = io.read_set_from_file(f, 'hdf5', close_file=True)
 
-i = 1
-n = '%.2f' % (stellar_masses_p.value_in(units.MSun)[i])
-s = str(n) + r'$M_{\odot}$'
-#ax.annotate(s, (sizes_p.value_in(units.au)[i], m_mjup_p[i]), xytext=(sizes_p.value_in(units.au)[i]*1.02, m_mjup_p[i]),
-#                    fontsize=14)
+        # Take only the small stars
+        small_stars = stars[stars.stellar_mass.value_in(units.MSun) <= 1.9]
 
-i = 2
-n = '%.2f' % (stellar_masses_p.value_in(units.MSun)[i])
-s = str(n) + r'$M_{\odot}$'
-#ax.annotate(s, (sizes_p.value_in(units.au)[i], m_mjup_p[i]), xytext=(sizes_p.value_in(units.au)[i]*1.02, m_mjup_p[i]),
-#                    fontsize=14)
+        sizes, masses = 2 * small_stars.disk_radius.value_in(units.au), small_stars.disk_mass.value_in(units.MJupiter)
 
-i = len(stellar_masses_p) - 1
-n = '%.2f' % (stellar_masses_p.value_in(units.MSun)[i])
-s = str(n) + r'$M_{\odot}$'
-#ax.annotate(s, (sizes_p.value_in(units.au)[i], m_mjup_p[i]), xytext=(sizes_p.value_in(units.au)[i]*1.02, m_mjup_p[i]),
-#                    fontsize=14)
+        ax.scatter(sizes, masses, s=100 * small_stars.stellar_mass.value_in(units.MSun),
+                   c=c, alpha=0.5, label=l)
+
+    ax.legend(loc='upper right', fontsize=20)
+    ax.set_title('N=100, c=50, different times')
+    ax.set_xlabel('Disk size [au]')
+    ax.set_ylabel(r'Disk mass [$M_{Jup}$]')
+    pyplot.show()
+    #fig.savefig('plot2.png')
 
 
-ax.axhline(y=(0.016 | units.MSun).value_in(units.MJupiter), c='r', ls=':')
-ax.text(0.0, (0.016 | units.MSun).value_in(units.MJupiter) + 0.5, 'MMSN', fontsize=18, color='r')
+def distance_from_star(files, labels, colors):
+    """ Plot average distance to bright star (au?) vs disk diameter (au)
 
-ax.set_title('N=10, 1 bright star, 2 Myr')
-ax.set_xlabel('Disk size [au]')
-ax.set_ylabel(r'Disk mass [$M_{Jup}$]')
-#pyplot.show()
-fig.savefig('plot2.png')
+    :param files: list with filenames to open
+    :param labels: list with labels for each file
+    :param colors: list with colors for each file
+    """
+    fig = pyplot.figure(figsize=(12, 8))
+    ax = pyplot.gca()
+
+    for f, l, c in zip(files, labels, colors):
+        stars = io.read_set_from_file(f, 'hdf5', close_file=True)
+
+        # Take only the small stars
+        small_stars = stars[stars.stellar_mass.value_in(units.MSun) <= 1.9]
+
+        sizes, masses = 2 * small_stars.disk_radius.value_in(units.au), small_stars.disk_mass.value_in(units.MJupiter)
+
+        ax.scatter(sizes, masses, s=100 * small_stars.stellar_mass.value_in(units.MSun),
+                   c=c, alpha=0.5, label=l)
+
+    ax.legend(loc='upper right', fontsize=20)
+    ax.set_title('N=100, c=50, different times')
+    ax.set_xlabel('Disk size [au]')
+    ax.set_ylabel(r'Disk mass [$M_{Jup}$]')
+    pyplot.show()
+    #fig.savefig('plot2.png')
+
+
+def surviving_disks(paths, N, cells, labels, colors):
+    """ Plot percentage of surviving disks at different times.
+
+    :param path: path to results file
+    :param N: number of stars, for results paths
+    :param c: number of disk cells, for results paths
+    :param labels: list with labels for each file
+    :param colors: list with colors for each file
+    """
+    times = [0.0, 0.5, 1.0, 1.5, 2.0, 2.5] #range(3)  # Time in Myr
+    fig = pyplot.figure(figsize=(12, 8))
+    ax = pyplot.gca()
+
+    for p, l, c in zip(paths, labels, colors):
+        total_disks = 0
+        disk_p = []
+        for t in times:
+            f = '{0}N{1}_c{2}/0/N{1}_t{3}.hdf5'.format(p, N, cells, t)
+            stars = io.read_set_from_file(f, 'hdf5', close_file=True)
+
+            # Take only the small stars
+            small_stars = stars[stars.stellar_mass.value_in(units.MSun) <= 1.9]
+            disked_stars = small_stars[small_stars.dispersed == False]
+
+            if t == 0:
+                total_disks = len(disked_stars)
+
+            disk_p.append(len(disked_stars)/total_disks)
+
+        ax.plot(times, disk_p)
+
+    ax.legend(loc='upper right', fontsize=20)
+    ax.set_title('N=100, c=50, different times')
+    ax.set_xlabel('Time [Myr]')
+    ax.set_ylabel(r'Fraction of disks remaining')
+    ax.set_xlim(left=0.0)
+    ax.set_ylim(0, 1)
+    pyplot.show()
+    #fig.savefig('plot2.png')
+
+
+
+
+def cdfs(files, labels, colors):
+    """ Plot cumulative distributions of disk sizes (au).
+
+    :param files: list with filenames to open
+    :param labels: list with labels for each file
+    :param colors: list with colors for each file
+    """
+    fig = pyplot.figure(figsize=(12, 8))
+    ax = pyplot.gca()
+
+    for f, l, c in zip(files, labels, colors):
+        stars = io.read_set_from_file(f, 'hdf5', close_file=True)
+
+        # Take only the small stars
+        small_stars = stars[stars.stellar_mass.value_in(units.MSun) <= 1.9]
+
+        sizes, masses = 2 * small_stars.disk_radius.value_in(units.au), small_stars.disk_mass.value_in(units.MJupiter)
+
+        ax.scatter(sizes, masses, s=100 * small_stars.stellar_mass.value_in(units.MSun),
+                   c=c, alpha=0.5, label=l)
+
+    ax.legend(loc='upper right', fontsize=20)
+    ax.set_title('N=100, c=50, different times')
+    ax.set_xlabel('Disk size [au]')
+    ax.set_ylabel(r'Disk mass [$M_{Jup}$]')
+    pyplot.show()
+    #fig.savefig('plot2.png')
+
+
+
+def main():
+    files = ['results/king_N100_c50/0/N100_t3.25.hdf5',
+             'results/plummer_N100_c50/0/N100_t0.7.hdf5',
+             'results/fractal_N100_c50/0/N100_t0.hdf5']
+
+    labels = ["King, Plummer, Fractal"]
+    plot_colors = ["#638ccc", "#ca5670", "#c57c3c", "#72a555", "#ab62c0"]
+
+    #size_vs_mass(files, labels, plot_colors)
+    #distance_from_star()
+
+    paths = ['results/king_',
+             'results/king_',
+             'results/king_']
+
+    surviving_disks(paths, 100, 50, labels, plot_colors)
+
+
+def new_option_parser():
+    from amuse.units.optparse import OptionParser
+    result = OptionParser()
+
+    # Simulation parameters
+    result.add_option("-n", dest="run_number", type="int", default=0,
+                      help="run number [%default]")
+    result.add_option("-s", dest="save_path", type="string", default='.',
+                      help="path to save the results [%default]")
+    result.add_option("-i", dest="save_interval", type="int", default=50000 | units.yr,
+                      help="time interval of saving a snapshot of the cluster [%default]")
+
+    # Cluster parameters
+    result.add_option("-N", dest="N", type="int", default=2000,
+                      help="number of stars [%default]")
+    result.add_option("-R", dest="Rvir", type="float",
+                      unit=units.parsec, default=0.5,
+                      help="cluster virial radius [%default]")
+    result.add_option("-Q", dest="Qvir", type="float", default=0.5,
+                      help="virial ratio [%default]")
+
+    # Disk parameters
+    result.add_option("-a", dest="alpha", type="float", default=5E-3,
+                      help="turbulence parameter [%default]")
+    result.add_option("-c", dest="ncells", type="int", default=100,
+                      help="Number of cells to be used in vader disk [%default]")
+
+    # Time parameters
+    result.add_option("-I", dest="t_ini", type="int", default=0 | units.yr,
+                      help="initial time [%default]")
+    result.add_option("-t", dest="dt", type="int", default=1000 | units.yr,
+                      help="dt for simulation [%default]")
+    result.add_option("-e", dest="t_end", type="float", default=2 | units.Myr,
+                      help="end time of the simulation [%default]")
+
+    return result
+
+
+if __name__ == '__main__':
+    o, arguments = new_option_parser().parse_args()
+    main()
+
