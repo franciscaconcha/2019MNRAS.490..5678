33b7784aaef68f1ca60d73bb3f746fe1d570fab8
Francisca <fran.concha.r@gmail.com>
2019-04-01 16:33:28 +0200

    Adding more figures

--- src/plots.py
+++ src/plots.py
@@ -7,9 +7,9 @@ from amuse.lab import *
 from amuse import io
 
 
-rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 24, })
+rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 22, })
 rc('text', usetex=True)
-rc('axes', labelsize=26)  # fontsize of the x and y labels
+rc('axes', labelsize=22)  # fontsize of the x and y labels
 matplotlib.rcParams['xtick.major.pad'] = 8  # to avoid overlapping x/y labels
 matplotlib.rcParams['ytick.major.pad'] = 8  # to avoid overlapping x/y labels
 
@@ -126,6 +126,7 @@ def size_vs_distance_from_star(paths, t_end, N, labels, colors, density):
 
 
 def mass_loss_distribution(open_path, save_path, t, N, colors, density):
+    from matplotlib.colors import LinearSegmentedColormap
     fig = pyplot.figure(figsize=(13, 12))
     ax = pyplot.gca()
     #fig, ax = pyplot.subplots(1, 2, figsize=(13, 12))#, gridspec_kw={"width_ratios": [12, 1]})
@@ -147,22 +148,40 @@ def mass_loss_distribution(open_path, save_path, t, N, colors, density):
                               (numpy.pi * small_stars.disk_radius.value_in(units.au) ** 2) > density]
     disked_stars = small_stars[small_stars.dispersed == False]
 
-    mass_loss_trunc = disked_stars.truncation_mass_loss.value_in(units.MSun)
-    mass_loss_photoevap = disked_stars.photoevap_mass_loss.value_in(units.MSun)
+    final_mass_loss_trunc = disked_stars.truncation_mass_loss.value_in(units.MSun)
+    final_mass_loss_photoevap = disked_stars.photoevap_mass_loss.value_in(units.MSun)
 
-    zero = numpy.zeros((len(mass_loss_trunc)))
+    #zero = numpy.zeros((len(mass_loss_trunc)))
     # Negative values for colormap: mass loss due to dynamical truncations.
     # Positive values for colormap: mass loss due to photoevaporation.
-    mass_loss_map = zero - mass_loss_trunc + mass_loss_photoevap
+    #mass_loss_map = zero - mass_loss_trunc + mass_loss_photoevap
 
-    minima = -max(mass_loss_trunc)
-    maxima = max(mass_loss_photoevap)
-    print minima, maxima
+    #minima = max(mass_loss_trunc)
+    #maxima = max(mass_loss_photoevap)
+    #print minima, maxima, min(mass_loss_photoevap)
 
-    colormap = matplotlib.cm.YlGnBu
+    #colormap = matplotlib.cm.coolwarm
 
-    norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
-    mapper = matplotlib.cm.ScalarMappable(norm=norm, cmap=colormap)
+    #norm = matplotlib.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
+    #mapper = matplotlib.cm.ScalarMappable(norm=norm, cmap=colormap)
+
+    cdict2 = {'red': ((0.0, 0.0, 0.0),
+                      (0.5, 0.0, 0.1),
+                      (1.0, 1.0, 1.0)),
+
+              'green': ((0.0, 0.0, 0.0),
+                        (1.0, 0.0, 0.0)),
+
+              'blue': ((0.0, 0.0, 1.0),
+                       (0.5, 0.1, 0.0),
+                       (1.0, 0.0, 0.0))
+              }
+
+    blue_red2 = LinearSegmentedColormap('BlueRed2', cdict2)
+    pyplot.register_cmap(cmap=blue_red2)
+    #pyplot.rcParams['image.cmap'] = 'BlueRed2'
+
+    colormap = pyplot.get_cmap('BlueRed2')
 
     #ax2 = fig.add_axes([0.95, 0.1, 0.03, 0.8])
     #cb = matplotlib.colorbar.ColorbarBase(ax2, cmap=matplotlib.cm.coolwarm,
@@ -183,6 +202,18 @@ def mass_loss_distribution(open_path, save_path, t, N, colors, density):
 
         bright_stars = stars[stars.stellar_mass.value_in(units.MSun) > 1.9]
 
+        # Open final files
+        f = '{0}/N{1}_t{2}.hdf5'.format(open_path, N, 10.0)
+        stars = io.read_set_from_file(f, 'hdf5', close_file=True)
+
+        # Take only the small stars
+        final_stars = []
+        for s in stars:
+            if s in disked_stars:
+                final_stars
+        final_mass_loss_trunc = final_stars.truncation_mass_loss.value_in(units.MSun)
+        final_mass_loss_photoevap = final.photoevap_mass_loss.value_in(units.MSun)
+
         if t == 0.0:  # Have to do this here because of a mess up when saving... will not be needed for future results
             mass_loss_trunc = numpy.zeros((len(disked_stars)))
             mass_loss_photoevap = numpy.zeros((len(disked_stars)))
@@ -191,11 +222,12 @@ def mass_loss_distribution(open_path, save_path, t, N, colors, density):
             mass_loss_photoevap = disked_stars.photoevap_mass_loss.value_in(units.MSun)
 
         zero = numpy.zeros((len(mass_loss_trunc)))
-        plot_mass_loss_map = zero - mass_loss_trunc + mass_loss_photoevap
+        plot_mass_loss_map = (zero - mass_loss_trunc + mass_loss_photoevap) / (final_mass_loss_photoevap +final_mass_loss_trunc)
+        #print plot_mass_loss_map[:10]
 
         pyplot.clf()
         ax = pyplot.gca()
-        ax.set_title('N={0}, c=100, t={1} Myr'.format(N, t))
+        ax.set_title('N={0}, c=100, t={1:.2f} Myr'.format(N, t))
         ax.set_xlabel('x [parsec]')
         ax.set_ylabel('y [parsec]')
         ax.set_xlim(-1.5, 1.5)
@@ -203,8 +235,8 @@ def mass_loss_distribution(open_path, save_path, t, N, colors, density):
 
         # Disks
         ml = ax.scatter(disked_stars.x.value_in(units.parsec), disked_stars.y.value_in(units.parsec),
-                        s=disked_stars.disk_radius.value_in(units.au), norm=norm,
-                        c=plot_mass_loss_map, cmap=colormap, alpha=0.5)
+                        s=disked_stars.disk_radius.value_in(units.au), #norm=norm,
+                        c=plot_mass_loss_map, cmap=colormap, alpha=0.5, vmin=-1, vmax=1)
 
         # Bright stars
         ax.scatter(bright_stars.x.value_in(units.parsec), bright_stars.y.value_in(units.parsec),
@@ -213,16 +245,17 @@ def mass_loss_distribution(open_path, save_path, t, N, colors, density):
         #ax.legend(loc='upper right', fontsize=20)
         pyplot.colorbar(mappable=ml)
         #pyplot.show()
-        fig.savefig('{0}/plot{1:.2f}.png'.format(save_path, t))
+        #fig.savefig('{0}/plot{1:.2f}.png'.format(save_path, t))
+        fig.savefig('{0}/plot{1}.png'.format(save_path, numpy.where(times==t)[0][0]))
 
 
-def mass_loss_in_time(open_path, save_path, N):  # For one star
+def size_in_time(open_path, save_path, N, i):  # For one star
     #fig = pyplot.figure(figsize=(12, 8))
     #ax = pyplot.gca()
     fig, (ax, ax2) = pyplot.subplots(2, 1, figsize=(12, 10))#, gridspec_kw={"width_ratios": [12, 1]})
-    ax.set_title('Mass loss in time')
+    ax.set_title('Distance to nearest bright stars')
     ax.set_xlabel('Time [Myr]')
-    ax.set_ylabel('Mass loss [MSun]')
+    ax.set_ylabel('Distance [parsec]')
 
     ax2.set_title('Disk size')
     ax2.set_xlabel('Time [Myr]')
@@ -234,32 +267,164 @@ def mass_loss_in_time(open_path, save_path, N):  # For one star
 
     #ax2 = ax.twinx()
 
-    i = 55
+    times = numpy.arange(0.0, 10.05, 0.05)
+
+    disk_size, distance_to_bright = [], []
+
+    for t in times:
+        f = '{0}/N{1}_t{2}.hdf5'.format(open_path, N, t)
+        stars = io.read_set_from_file(f, 'hdf5', close_file=True)
+        bright_stars = stars[stars.stellar_mass.value_in(units.MSun) > 1.9]
+        s = stars[i]
+
+        distances = []
+
+        for bs in bright_stars:
+            distances.append(distance(bs, s).value_in(units.parsec))
+
+        nearest_bs = min(distances)
+
+        distance_to_bright.append(nearest_bs)
+        disk_size.append(2 * s.disk_radius.value_in(units.au))
+
+    disk_size[1] = disk_size[0]  # this has to do with the saving issue, will fix soon
+    disk_size[2] = disk_size[0]
+
+    ax.plot(times, distance_to_bright)
+    ax2.plot(times, disk_size)
+
+    #ax.legend(loc='upper left', fontsize=20)
+
+    pyplot.tight_layout()
+    #pyplot.show()
+
+
+def mass_loss_in_time(open_path, save_path, N, i):  # For one star
+    #fig = pyplot.figure(figsize=(12, 8))
+    #ax = pyplot.gca()
+    fig, (ax, ax2) = pyplot.subplots(2, 1, figsize=(12, 10))#, gridspec_kw={"width_ratios": [12, 1]})
+    ax.set_title('Mass loss in time')
+    ax.set_xlabel('Time [Myr]')
+    ax.set_ylabel('Mass loss [MSun]')
+
+    ax2.set_title('Distance to nearest bright stars')
+    ax2.set_xlabel('Time [Myr]')
+    ax2.set_ylabel('Distance [parsec]')
+
+    #ax.set_xlim(-1.5, 1.5)
+    #ax.set_ylim(-1.5, 1.5)
+    #ax.axis('square')
+    #pyplot.axes(ax).set_aspect('equal', adjustable='box')
+
+    #ax2 = ax.twinx()
 
     times = numpy.arange(0.0, 10.05, 0.05)
 
-    pe, dt, ds = [], [], []
+    photoevap_mass_loss, dyn_trunc_mass_loss, disk_size, distance_to_bright = [], [], [], []
 
     for t in times:
         f = '{0}/N{1}_t{2}.hdf5'.format(open_path, N, t)
         stars = io.read_set_from_file(f, 'hdf5', close_file=True)
         s = stars[i]
 
-        pe.append(s.photoevap_mass_loss.value_in(units.MSun))
-        dt.append(s.truncation_mass_loss.value_in(units.MSun))
-        ds.append(2 * s.disk_radius.value_in(units.au))
+        photoevap_mass_loss.append(s.photoevap_mass_loss.value_in(units.MSun))
+        dyn_trunc_mass_loss.append(s.truncation_mass_loss.value_in(units.MSun))
+        disk_size.append(2 * s.disk_radius.value_in(units.au))
 
-    ds[1] = ds[0]  # this has to do with the saving issue, will fix soon
-    ds[2] = ds[0]
+    disk_size[1] = disk_size[0]  # this has to do with the saving issue, will fix soon
+    disk_size[2] = disk_size[0]
 
-    ax.plot(times, pe, c='r', label="photoevap")
-    ax.plot(times, dt, c='b', label="dyn trunc")
-    ax2.plot(times, ds, c='k')
+    ax.plot(times, photoevap_mass_loss, c='r', label="photoevap")
+    ax.plot(times, dyn_trunc_mass_loss, c='b', label="dyn trunc")
+    ax2.plot(times, disk_size, c='k')
+
+    ax.legend(loc='upper left', fontsize=20)
+
+    pyplot.tight_layout()
+    #pyplot.show()
+
+
+def single_star(open_path, save_path, N, i):
+    #fig = pyplot.figure(figsize=(12, 8))
+    #ax = pyplot.gca()
+    fig, (ax, ax2, ax3) = pyplot.subplots(3, 1, figsize=(13, 14))#, gridspec_kw={"width_ratios": [12, 1]})
+    ax.set_title('Mass loss in time')
+    #ax.set_xlabel('Time [Myr]')
+    ax.set_ylabel('Mass loss [MSun]')
+
+    ax2.set_title('Distance to nearest bright star')
+    #ax2.set_xlabel('Time [Myr]')
+    ax2.set_ylabel('Distance [parsec]')
+
+    ax3.set_title('Disk size')
+    ax3.set_xlabel('Time [Myr]')
+    ax3.set_ylabel('Disk size [au]')
+
+    #ax.set_xlim(-1.5, 1.5)
+    #ax.set_ylim(-1.5, 1.5)
+    #ax.axis('square')
+    #pyplot.axes(ax).set_aspect('equal', adjustable='box')
+
+    #ax2 = ax.twinx()
+
+    times = numpy.arange(0.0, 10.05, 0.05)
+
+    photoevap_mass_loss, dyn_trunc_mass_loss, disk_size, distance_to_bright, bs_masses = [], [], [], [], []
+
+    for t in times:
+        f = '{0}/N{1}_t{2}.hdf5'.format(open_path, N, t)
+        stars = io.read_set_from_file(f, 'hdf5', close_file=True)
+        bright_stars = stars[stars.stellar_mass.value_in(units.MSun) > 1.9]
+        s = stars[i]
+
+        distances = []
+
+        for bs in bright_stars:
+            distances.append(distance(bs, s).value_in(units.parsec))
+
+        nearest_bs = min(distances)
+        distance_to_bright.append(nearest_bs)
+
+        index_nearest_bs = distances.index(nearest_bs)
+        bs_masses.append(bright_stars[index_nearest_bs].stellar_mass.value_in(units.MSun))
+
+        photoevap_mass_loss.append(s.photoevap_mass_loss.value_in(units.MSun))
+        dyn_trunc_mass_loss.append(s.truncation_mass_loss.value_in(units.MSun))
+        disk_size.append(2 * s.disk_radius.value_in(units.au))
+
+    disk_size[1] = disk_size[0]  # this has to do with the saving issue, will fix soon
+    disk_size[2] = disk_size[0]
+
+    ax.plot(times, photoevap_mass_loss, c='r', label="photoevap")
+    ax.plot(times, dyn_trunc_mass_loss, c='b', label="dyn trunc")
+    ax2.plot(times, distance_to_bright, c='k', alpha=0.5)
+
+    scattered_times = [times[i] for i in xrange(0, len(times), 10)]
+    scattered_distances = [distance_to_bright[i] for i in xrange(0, len(distance_to_bright), 10)]
+    scattered_masses = [bs_masses[i] for i in xrange(0, len(bs_masses), 10)]
+
+    ax2.scatter(scattered_times, scattered_distances, s=scattered_masses, marker ="*", c='r')
+
+    ax3.plot(times, disk_size, c='k')
+
+    import matplotlib.lines as mlines
+    sorted_masses = bright_stars.stellar_mass.value_in(units.MSun)  # Only keeping the last 3 but it's fine for what I need
+    sorted_masses.sort()
+    print sorted_masses
+
+    bs1 = mlines.Line2D([], [], color='red', marker='*', linestyle='None',
+                              markersize=sorted_masses[0], label=r'{0:.2} $M_\odot$'.format(sorted_masses[0]))
+    bs2 = mlines.Line2D([], [], color='red', marker='*', linestyle='None',
+                              markersize=sorted_masses[1], label=r'{0:.2} $M_\odot$'.format(sorted_masses[1]))
+    bs3 = mlines.Line2D([], [], color='red', marker='*', linestyle='None',
+                              markersize=0.1 * sorted_masses[2], label=r'{0} $M_\odot$'.format(sorted_masses[2]))
+    ax2.legend(handles=[bs1, bs2, bs3], loc="lower left", fontsize=12)
 
     ax.legend(loc='upper left', fontsize=20)
 
     pyplot.tight_layout()
     pyplot.show()
+
 
 
 def distance_from_center(paths, t, N, labels, colors, density):
@@ -423,7 +588,7 @@ def plot_cluster(path, t, N, colors, density):
     # fig.savefig('plot2.png')
 
 
-def main(run_number, save_path, time, N, distribution, ncells, density):
+def main(run_number, save_path, time, N, distribution, ncells, density, i):
 
     files = ['results/king_N100_c100_3/0/N100_t10.0.hdf5',
              'results/plummer_N100_c100_3/0/N100_t10.0.hdf5']#,
@@ -443,8 +608,9 @@ def main(run_number, save_path, time, N, distribution, ncells, density):
     path = 'results/{0}_N{1}_c{2}_3/0/'.format(distribution, N, ncells)
     #plot_cluster(path, time, N, plot_colors, density)
     #mass_loss_distribution(path, save_path, time, N, plot_colors, density)
-    mass_loss_in_time(path, save_path, N)
-
+    #mass_loss_in_time(path, save_path, N, i)
+    #size_in_time(path, save_path, N, i)
+    single_star(path, save_path, N, i)
 
 def new_option_parser():
     from amuse.units.optparse import OptionParser
@@ -469,6 +635,8 @@ def new_option_parser():
                       help="Number of cells to be used in vader disk [%default]")
     result.add_option("-d", dest="density", type="float", default=1E-9,
                       help="Density limit for disk surface [%default]")
+    result.add_option("-i", dest="i", type="int", default=0,
+                      help="Individual star to plot [%default]")
 
     return result
 
